28128946b872c55f5602086e5df6f4ea745570fe
JeanBarboza <jeanbarboza.2k@gmail.com>
2021-08-21 12:01:14 -0300

    Update button exit
    Fiz um fix no botão seguido a orientação do professor, agora sai da aplicação.

--- Assets/Scripts/Utils/ButtonExit.cs
+++ Assets/Scripts/Utils/ButtonExit.cs
@@ -6,6 +6,11 @@ public class ButtonExit : MonoBehaviour
 {
     public void Quit()
     {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else   
         Application.Quit();
+#endif
     }
 }
