5f41baaceaedc9a14d4775238551bdcf87cd9c3e
JeanBarboza <jeanbarboza.2k@gmail.com>
2021-08-23 18:58:12 -0300

    Add health base and enemy base to the project

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigibody;
 
+    [Header("Speed setup")]
     public Vector2 friction = new Vector2(.1f, 0);
-
     public float speed;
     public float speedRun;
-
     public float forceJump = 2;
 
-    private float _currentSpeed;
+    [Header("Animation setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.6f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
 
+    private float _currentSpeed;
     private bool _isRunning = false;
 
     private void Update()
@@ -56,6 +61,19 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigibody.velocity = Vector2.up * forceJump;
+            myRigibody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigibody.transform);
+
+            HandleScaleJump();
         }
+            
     }
+
+    private void HandleScaleJump()
+    {
+        myRigibody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigibody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
 }
