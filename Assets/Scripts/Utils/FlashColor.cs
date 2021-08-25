4470e4a683f11ec25dfbc0a88f50cc283d1409bc
JeanBarboza <jeanbarboza.2k@gmail.com>
2021-08-25 19:19:04 -0300

    Add flash color in Astronaut

---
+++ Assets/Scripts/Utils/FlashColor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlashColor : MonoBehaviour
+{
+    public List<SpriteRenderer> spriteRenderers;
+    public Color color = Color.red;
+    public float duration = .2f;
+
+    private Tween _currentTween;
+
+    private void OnValidate()
+    {
+        spriteRenderers = new List<SpriteRenderer>();
+        foreach(var child in transform.GetComponentsInChildren<SpriteRenderer>())
+        {
+            spriteRenderers.Add(child);
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            Flash();
+        }
+    }
+    public void Flash()
+    {
+        if(_currentTween != null)
+        {
+            _currentTween.Kill();
+            spriteRenderers.ForEach(i => i.color = Color.white);
+        }
+
+        foreach(var c in spriteRenderers)
+        {
+            _currentTween = c.DOColor(color, duration).SetLoops(2, LoopType.Yoyo);
+        }
+    }
+}
